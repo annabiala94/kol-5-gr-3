2249e7bf85b300f11e5448fe97c6129f4c3056b3
Bartlomiej Oberc <barteko21993@gmail.com>
2015-05-08 09:45:51 +0200

    ostatni commit

--- spec/barteko2/Tools/TrapezSpec.php
+++ spec/barteko2/Tools/TrapezSpec.php
@@ -11,4 +11,11 @@ class TrapezSpec extends ObjectBehavior
     {
         $this->shouldHaveType('barteko2\Tools\Trapez');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(967)->getH()->shouldReturn(967);
+    }
 }
--- src/barteko2/Tools/Trapez.php
+++ src/barteko2/Tools/Trapez.php
@@ -4,4 +4,50 @@ namespace barteko2\Tools;
 
 class Trapez
 {
+
+    private $a;
+    private $b;
+
+    public function setA($a)
+    {
+        $this->a = $a;
+
+        return $this;
+    }
+
+    public function setB($b)
+    {
+        $this->b = $b;
+
+        return $this;
+    }
+    public function setH($h)
+    {
+        $this->h = $h;
+
+        return $this;
+    }
+
+    public function getA()
+    {
+        return $this->a;
+    }
+
+    public function getB()
+    {
+        return $this->b;
+    }
+    public function getH()
+    {
+        return $this->h;
+    }
+    function it_should_trapez_pole()
+{
+    $this->setA(5)->setB(7)->setH(2)->poleTrapezu()->shouldReturn(12);
+}
+    
+public function poleTrapezu()
+{
+    return $this->a + $this->b * $this->h/2;
+}
 }
