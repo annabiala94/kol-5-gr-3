db3f8b1e6c972d2ec28ff2bc85ff9a72bc2a3e92
Anna Biala <aniamuzyka121@wp.pl>
2015-05-08 10:16:04 +0200

    trapez

--- spec/annabiala94/Tools/TrapezSpec.php
+++ spec/annabiala94/Tools/TrapezSpec.php
@@ -11,4 +11,15 @@ class TrapezSpec extends ObjectBehavior
     {
         $this->shouldHaveType('annabiala94\Tools\Trapez');
     }
+    
+      function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(345)->getH()->shouldReturn(345);
+    }
+    function it_should_calculate_pole()
+    {
+       $this->setA(5)->setB(5)->setH(7)->poletrapezu()->shouldReturn(35);
+    }
 }
--- src/annabiala94/Tools/Trapez.php
+++ src/annabiala94/Tools/Trapez.php
@@ -4,4 +4,39 @@ namespace annabiala94\Tools;
 
 class Trapez
 {
+    private $a;
+    private $b;
+    private $h;
+    public function setA($a)
+    {
+        $this->a = $a;
+        return $this;
+    }
+    public function getA()
+    {
+        return $this->a;
+    }
+    public function setB($b)
+    {
+        $this->b = $b;
+        return $this;
+    }
+    public function getB()
+    {
+        return $this->b;
+    }
+    public function setH($h)
+    {
+        $this->h = $h;
+        return $this;
+    }
+     public function getH()
+    {
+        return $this->h;
+    }
+    public function poletrapezu()
+    {
+    return ($this->a + $this->b) * $this->h /2;
+    }
+   
 }
