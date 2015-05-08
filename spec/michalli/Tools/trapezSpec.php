ab88448691e4831e14587094df94ff811e46d485
michalli <mikemystify@gmail.com>
2015-05-08 09:51:29 +0200

    michalli/trapez: setter/getter

--- spec/michalli/Tools/trapezSpec.php
+++ spec/michalli/Tools/trapezSpec.php
@@ -11,4 +11,10 @@ class trapezSpec extends ObjectBehavior
     {
         $this->shouldHaveType('michalli\Tools\trapez');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+        $this->setH(456)->getH()->shouldReturn(456);
+    }
 }
