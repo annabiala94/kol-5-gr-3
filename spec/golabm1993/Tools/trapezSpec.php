dd1a94817ba146d1b5cedcfcb10beb20dded9deb
Malgorzata Golab <golabm236@gmail.com>
2015-05-08 09:35:11 +0200

    golabm1993/trapez: wygenerowana klasa - phpspec

---
+++ spec/golabm1993/Tools/trapezSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace spec\golabm1993\Tools;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class trapezSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('golabm1993\Tools\trapez');
+    }
+}
